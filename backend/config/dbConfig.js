43d59b501c5f09b94a04d6630722b83317f4d4d8
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2025-03-30 15:43:28 +0530

    Now fix db url error

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,7 +5,7 @@ const mongodbAtlasUrl = process.env.MONGODB_ATLAS_URL;
 // const mongodbLocalUrl = "mongodb://127.0.0.1:27017/zerodha";
 
 async function connectDb() {
-    await connect(mongodbLocalUrl || mongodbAtlasUrl);
+    await connect(mongodbAtlasUrl);
 }
 connectDb().then(() => console.log("Database connected!"))
     .catch((err) => console.log("Db connection failed :-", err.message));
